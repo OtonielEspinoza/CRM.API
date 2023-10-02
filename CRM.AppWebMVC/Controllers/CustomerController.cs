ef90859d7c2e2c2c4a61a8ff73b89320ac5740ba
Otoniel Esteban Espinoza Contreras <esteban15espinosa@outlook.com>
2023-10-01 22:16:44 -0600

    mvc con error

---
+++ CRM.AppWebMVC/Controllers/CustomerController.cs
@@ -0,0 +1,144 @@
+﻿using CRM.DTOs.CustomerDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.AppWebMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly HttpClient _httpClientCRM;
+
+        public CustomerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientCRM = httpClientFactory.CreateClient("CRM");
+        }
+
+        public async Task<IActionResult> Index(SearchQueryCustomerDTO searchQueryCustomerDTO, int CountRow = 0)
+        {
+            if (searchQueryCustomerDTO.SendRowCount == 0)
+                searchQueryCustomerDTO.SendRowCount = 2;
+            if (searchQueryCustomerDTO.Take == 0)
+                searchQueryCustomerDTO.Take = 10;
+
+            var result = new SearchResultCustomerDTO();
+
+            var response = await _httpClientCRM.PostAsJsonAsync("/customer/search", searchQueryCustomerDTO);
+            if (response.IsSuccessStatusCode)
+                result = await response.Content.ReadFromJsonAsync<SearchResultCustomerDTO>();
+
+            result = result != null ? result : new SearchResultCustomerDTO();
+
+            if (result.CountRow == 0 && searchQueryCustomerDTO.SendRowCount == 1)
+                result.CountRow = CountRow;
+
+            ViewBag.CountRow = result.CountRow;
+            searchQueryCustomerDTO.SendRowCount = 0;
+            ViewBag.SearchQuery = searchQueryCustomerDTO;
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var result = new GetIdResultCustomerDTO();
+
+            var response = await _httpClientCRM.GetAsync("/customer/" + id);
+
+            if (response.IsSuccessStatusCode)
+                result = await response.Content.ReadFromJsonAsync<GetIdResultCustomerDTO>();
+            return View(result ?? new GetIdResultCustomerDTO());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateCustomerDTO createCustomerDTO)
+        {
+            try
+            {
+                var response = await _httpClientCRM.PostAsJsonAsync("/customer", createCustomerDTO);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ViewBag.Error = "Error al intentar guardar el registro";
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var result = new GetIdResultCustomerDTO();
+            var response = await _httpClientCRM.GetAsync("/customer/" + id);
+
+            if (response.IsSuccessStatusCode)
+                result = await response.Content.ReadFromJsonAsync<GetIdResultCustomerDTO>();
+
+            return View(new EditCustomerDTO(result ?? new GetIdResultCustomerDTO()));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, EditCustomerDTO editCustomerDTO)
+        {
+            try
+            {
+                var response = await _httpClientCRM.PutAsJsonAsync("/customer", editCustomerDTO);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ViewBag.Error = "Error al intentar editar el registro";
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = new GetIdResultCustomerDTO();
+            var response = await _httpClientCRM.GetAsync("/customer/" + id);
+
+            if (response.IsSuccessStatusCode)
+                result = await response.Content.ReadFromJsonAsync<GetIdResultCustomerDTO>();
+            return View(result ?? new GetIdResultCustomerDTO());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, GetIdResultCustomerDTO getIdResultCustomerDTO)
+        {
+            try
+            {
+                var response = await _httpClientCRM.DeleteAsync("/customer/" + id);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ViewBag.Error = "Error al intentar eliminar el registro";
+                return View(getIdResultCustomerDTO);
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                return View(getIdResultCustomerDTO);
+            }
+        }
+
+    }
+}
