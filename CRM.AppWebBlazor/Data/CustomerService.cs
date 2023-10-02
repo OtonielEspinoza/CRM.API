476ad5707039616401b45244a0e3ae3050261e3a
Otoniel Esteban Espinoza Contreras <esteban15espinosa@outlook.com>
2023-10-01 22:39:18 -0600

    finalizado blazor

---
+++ CRM.AppWebBlazor/Data/CustomerService.cs
@@ -0,0 +1,77 @@
+﻿using CRM.DTOs.CustomerDTOs;
+
+namespace CRM.AppWebBlazor.Data
+{
+    public class CustomerService
+    {
+        readonly HttpClient _httpClientCRM;
+
+        public CustomerService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientCRM = httpClientFactory.CreateClient("CRM");
+        }
+
+        public async Task<SearchResultCustomerDTO> Search(SearchQueryCustomerDTO searchQueryCustomerDTO)
+        {
+            var response = await _httpClientCRM.PostAsJsonAsync("/customer/search", searchQueryCustomerDTO);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<SearchResultCustomerDTO>();
+                return result ?? new SearchResultCustomerDTO();
+            }
+            return new SearchResultCustomerDTO();
+        }
+
+        public async Task<GetIdResultCustomerDTO> GetById(int id)
+        {
+            var response = await _httpClientCRM.GetAsync("/customer/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<GetIdResultCustomerDTO>();
+                return result ?? new GetIdResultCustomerDTO();
+            }
+            return new GetIdResultCustomerDTO();
+        }
+
+        public async Task<int> Create(CreateCustomerDTO createCustomerDTO)
+        {
+            int result = 0;
+            var response = await _httpClientCRM.PostAsJsonAsync("/customer", createCustomerDTO);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                if (int.TryParse(responseBody, out result) == false)
+                    result = 0;
+            }
+            return result;
+        }
+
+        public async Task<int> Edit(EditCustomerDTO editCustomerDTO)
+        {
+            int result = 0;
+            var response = await _httpClientCRM.PutAsJsonAsync("/customer", editCustomerDTO);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                if (int.TryParse(responseBody, out result) == false)
+                    result = 0;
+            }
+            return result;
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            int result = 0;
+            var response = await _httpClientCRM.DeleteAsync("/customer/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                if (int.TryParse(responseBody, out result) == false)
+                    result = 0;
+            }
+            return result;
+        }
+    }
+}
+
+
